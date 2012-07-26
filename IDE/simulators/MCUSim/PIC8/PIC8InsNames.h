f7c13100116af7d7495ce4a76e381a17ee7b4e44
Erik C <xchalu10@stud.fit.vutbr.cz>
2012-07-26 02:24:18 +0200

    + update in PIC8 instruction set

--- IDE/simulators/MCUSim/PIC8/PIC8InsNames.h
+++ IDE/simulators/MCUSim/PIC8/PIC8InsNames.h
@@ -6,6 +6,7 @@
  *
  * Copyright: See COPYING file that comes with this distribution.
  *
+ * @author Erik Chalupa <xchalu10@stud.fit.vutbr.cz>, (C) 2012
  * @author Martin Ošmera <martin.osmera@gmail.com>, (C) 2012
  * @ingroup PIC8
  * @file PIC8InsNames.h
@@ -22,7 +23,41 @@
 namespace PIC8InsNames {
 	enum Instructions {
 		INS_NONE = 0,
-
+		INS_ADDWF,
+		INS_ANDWF,
+		INS_CLRF,
+		INS_CLRW,
+		INS_COMF,
+		INS_DECF,
+		INS_DECFSZ,
+		INS_INCF,
+		INS_INCFSZ,
+		INS_IORWF,
+		INS_MOVF,
+		INS_MOVWF,
+		INS_NOP,
+		INS_RLF,
+		INS_RRF,
+		INS_SUBWF,
+		INS_SWAPF,
+		INS_XORWF,
+		INS_BCF,
+		INS_BSF,
+		INS_BTFSC,
+		INS_BTFSS,
+		INS_ADDLW,
+		INS_ANDLW,
+		INS_CALL,
+		INS_CLRWDT,
+		INS_GOTO,
+		INS_IORLW,
+		INS_MOVLW,
+		INS_RETFIE,
+		INS_RETLW,
+		INS_RETURN,
+		INS_SLEEP,
+		INS_SUBLW,
+		INS_XORLW,
 		INS__MAX__
 	};
 };
