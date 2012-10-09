14734326bcad6e9c4bdea0b8abb4fb97cff74a5e
Erik Chalupa <erik.chalupa@gmail.com>
2012-10-09 13:55:11 +0200

    - (GUI) wrong files in widgets/Highlighter

--- IDE/GUI/widgets/Highlighter/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rift/Plocha/gui/gitrep/src/IDE/GUI/widgets/Highlighter
-# Build directory: /home/rift/Plocha/gui/gitrep/src/IDE/GUI/widgets/Highlighter
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
