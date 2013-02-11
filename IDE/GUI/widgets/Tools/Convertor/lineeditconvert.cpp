5425b15c1b504005fc8fdd10a27a5c8b33f5d1bb
Erik C <xchalu10@stud.fit.vutbr.cz>
2013-02-11 16:16:39 +0100

    + (GUI) Convertor Tool updated (16bit now)

--- IDE/GUI/widgets/Tools/Convertor/lineeditconvert.cpp
+++ IDE/GUI/widgets/Tools/Convertor/lineeditconvert.cpp
@@ -6,6 +6,24 @@ LineEditConvert::LineEditConvert(QWidget *parent, int base)
 {
     connect(this, SIGNAL(textEdited(const QString &)), this, SLOT(catchEditSignal(const QString &)));
     this->base = base;
+    if (base == 2)
+    {
+        this->setInputMask("BBBBBBBBBBBBBBBB");
+        this->setText("0000000000000000");
+    }
+    else if (base == 10)
+    {
+        this->setInputMask("99999");
+        QIntValidator *dec32bitValidator = new QIntValidator(0, 65535, this);
+        this->setValidator(dec32bitValidator);
+        this->setText("00000");
+    }
+    else
+    {
+        this->setInputMask("HHHH");
+        this->setText("0000");
+    }
+    
 }
 
 
@@ -18,13 +36,38 @@ void LineEditConvert::setTextSlot(const QString &text, int base)
 {
     bool done;
     int number = text.toInt(&done, base);
+    qDebug () << "LineEditConvert: Number" << text << "of base" << base << "is" << number;
     if (done)
     {
         QString a = QString::number(number, this->base);
+        qDebug() << "LineEditConvert: Number converted to base" << this->base << "is" << a;
+        int length = 0;
+        if (this->base == 2)
+        {
+            length = 16 - a.length();
+        }
+        else if (this->base == 10)
+        {
+            length = 5 - a.length();
+        }
+        else
+        {
+            length = 4 - a.length();
+        }
+        while (length > 0)
+        {
+            a.prepend('0');
+            length--;
+        }
+        qDebug() << "LineEditConvert: Text set:" << a;
         this->setText(a);
     }
     else if (text == "")
-            this->setText("");
-        else
-            this->setText("INVALID NUMBER");
+    {
+        this->setText("0");
+    }
+    /*else
+    {
+        this->setText("INVALID NUMBER");
+    }*/
 }
