a8af38cf62f5ade287af2e1c8e9bc2d7e85a0af8
Martin Ošmera <martin.osmera@gmail.com>
2013-02-22 15:17:54 +0100

    .

--- IDE/compiler/main.cxx
+++ IDE/compiler/main.cxx
@@ -26,7 +26,7 @@ int main ( int argc, char ** argv )
 
     std::string filename = argv[1];
 
-    if ( true == compiler.compile(CompilerBase::LI_ASM, CompilerBase::TA_MCS51, &opts, filename) )
+    if ( true == compiler.compile ( CompilerBase::LI_ASM, CompilerBase::TA_AVR8, &opts, filename ) )
     {
         std::cout << "SUCCESS\n";
         return 0;
