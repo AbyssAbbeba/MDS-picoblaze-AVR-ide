ffd47766a7fdedfb6155773c2042324b13d10365
Martin Madron <pomerange@seznam.cz>
2013-06-03 17:43:19 +0200

    + testcases

---
+++ IDE/tests/compiler/assembler/PicoBlaze/TestSuiteSuc/testcases/001_logical.asm
@@ -0,0 +1,97 @@
+; Compiler test case for Assembler
+; instruction opcodes
+
+
+
+s1       EQU        04h
+s2       EQU        0x04
+s3       EQU        6
+s4       EQU        7
+
+
+Start:        
+
+
+; Logical
+        LOAD      s1,#01h
+        LOAD      s1,#5
+        LOAD      s1,#O5
+        LOAD      s1,#0x05
+        LOAD      s1,#0b101
+        LOAD      s1,#0b00000101
+        LOAD      s1,#5D
+        LOAD      s1,#5O
+        LOAD      s1,#5Q
+        LOAD      s1,#5h
+        LOAD      s1,#101b
+        LOAD      s1,#00000101b
+        
+        AND       s1,#5
+        AND       s1,#O5
+        AND       s1,#0x05
+        AND       s1,#0b101
+        AND       s1,#0b00000101
+        AND       s1,#5D
+        AND       s1,#5O
+        AND       s1,#5Q
+        AND       s1,#5h
+        AND       s1,#101b
+        AND       s1,#00000101b
+        
+        OR        s1,#5
+        OR        s1,#5
+        OR        s1,#O5
+        OR        s1,#0x05
+        OR        s1,#0b101
+        OR        s1,#0b00000101
+        OR        s1,#5D
+        OR        s1,#5O
+        OR        s1,#5Q
+        OR        s1,#5h
+        OR        s1,#101b
+        OR        s1,#00000101b
+        
+        XOR       s1,#5
+        XOR       s1,#5
+        XOR       s1,#O5
+        XOR       s1,#0x05
+        XOR       s1,#0b101
+        XOR       s1,#0b00000101
+        XOR       s1,#5D
+        XOR       s1,#5O
+        XOR       s1,#5Q
+        XOR       s1,#5h
+        XOR       s1,#101b
+        XOR       s1,#00000101b
+        
+        TEST      s1,#5
+        TEST      s1,#5
+        TEST      s1,#O5
+        TEST      s1,#0x05
+        TEST      s1,#0b101
+        TEST      s1,#0b00000101
+        TEST      s1,#5D
+        TEST      s1,#5O
+        TEST      s1,#5Q
+        TEST      s1,#5h
+        TEST      s1,#101b
+        TEST      s1,#00000101b
+
+        LOAD      s1,s2
+        AND       s1,s2
+        OR        s1,s2
+        XOR       s1,s2
+        TEST      s1,s2
+
+END
+
+  ;  JUMP aaa ADD sX,kk LOAD sX,kk SR0 sX
+;JUMP Z,aaa ADDCY sX,kk AND sX,kk SR1 sX
+;JUMP NZ,aaa SUB sX,kk OR sX,kk SRX sX
+;JUMP C,aaa SUBCY sX,kk XOR sX,kk SRA sX
+;JUMP NC,aaa COMPARE sX,kk TEST sX,kk RR sX
+;CALL aaa ADD sX,sY LOAD sX,sY SL0 sX
+;CALL Z,aaa ADDCY sX,sY AND sX,sY SL1 sX
+;CALL NZ,aaa SUB sX,sY OR sX,sY SLX sX
+;CALL C,aaa SUBCY sX,sY XOR sX,sY SLA sX
+;CALL NC,aaa COMPARE sX,sY TEST sX,sY RL sX
---
+++ IDE/tests/compiler/assembler/PicoBlaze/TestSuiteSuc/testcases/009_asm_SPI.asm
@@ -0,0 +1,287 @@
+
+bSDA_In         EQU    _PIGGY + _I2C0          ; gets SDA level in Bit0
+bSDA_0          EQU    _PIGGY + _I2C0          ; Pulls SDA to 0
+bSDA_Z          EQU    _PIGGY + _I2C1          ; Relases SDA to float to 1
+bSCL_0          EQU    _PIGGY + _I2C2          ; Pulls CSCL to 0
+bSCL_Z          EQU    _PIGGY + _I2C3          ; Releases SCL to float to 1
+
+; ---\              /--------\                                            /--
+;     \  SDA       /          \                                          /
+;      \----------/ \--------/ \----------------------------------------/
+; -------\            /----\                                         /-------
+;         \  SCL     /      \                                       /
+;          \--------/        \-------------------------------------/
+; | Start    |      Data bit     |                             |  Stop     |
+
+Init_I2C:       OUTPUT     sF, bSDA_Z              ; SDA = Z
+                OUTPUT     sF, bSCL_Z              ; SCL = Z
+                JUMP    Delay
+
+
+; -------------------------------------------------------------------------------
+; Routine to set up for and read four bytes from I2C device
+; -------------------------------------------------------------------------------
+
+ReadWrite4_I2C:
+                CALL    I2C_Start               ; Send Start, control byte and ack
+                INPUT    sC, s0                  ; Load device code for TX
+                SL0     sC                      ; add write flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s1                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                CALL    I2C_RepStart            ; Send repeated start, control byte and ack
+                INPUT    sC, s0                  ; Load device code for TX
+                SL1     sC                      ; add read flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                CALL    I2C_Recv                ; Read 8 bits of data and send ack
+                CALL    I2C_MAck
+                INPUT    s2, sC
+
+                CALL    I2C_Recv                ; Read next 8 bits of data and send ack
+                CALL    I2C_MAck
+                INPUT    s3, sC
+
+                CALL    I2C_Recv                ; Read 8 bits of data and send ack
+                CALL    I2C_MAck
+                INPUT    s4, sC
+
+                CALL    I2C_Recv                ; Read next 8 bits of data and send ack
+                CALL    I2C_MNAck
+                INPUT    s5, sC
+
+                JUMP    I2C_Stop                ; Send Stop
+
+; -------------------------------------------------------------------------------
+; Routine to set up for and write two bytes to I2C device
+; -------------------------------------------------------------------------------
+Write3_I2C:
+                CALL    I2C_Start               ; Send Start, control byte and ack
+                INPUT    sC, s0                  ; Load device code for TX
+                SL0     sC                      ; add write flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s1                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s2                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s3                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                JUMP    I2C_Stop                ; Send stop
+
+; -------------------------------------------------------------------------------
+; Routine to set up for and read two bytes from I2C device
+; -------------------------------------------------------------------------------
+
+Read2_I2C:
+                CALL    I2C_Start               ; Send start, control byte and ack
+                INPUT    sC, s0                  ; Load device code for TX
+                SL1     sC                      ; add read flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                CALL    I2C_Recv                ; Read 8 bits of data and send ack
+                CALL    I2C_MAck
+                INPUT    s2, sC
+
+                CALL    I2C_Recv                ; Read next 8 bits of data and send ack
+                CALL    I2C_MNAck
+                INPUT    s3, sC
+
+                JUMP    I2C_Stop                ; Send Stop
+
+; -------------------------------------------------------------------------------
+; Routine to set up for and write two bytes to I2C device
+; -------------------------------------------------------------------------------
+Write2_I2C:
+                CALL    I2C_Start               ; Send Start, control byte and ack
+                INPUT    sC, s0                  ; Load device code for TX
+                SL0     sC                      ; add write flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s1                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s2                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                JUMP    I2C_Stop                ; Send stop
+
+; -------------------------------------------------------------------------------
+; Routine to set up for and read one byte of I2C device
+; -------------------------------------------------------------------------------
+Read1_I2C:
+
+                CALL    I2C_Start               ; Send start, control byte and ack
+                INPUT    sC, s0                  ; Load address for TX
+                SL1     sC                      ; add read flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                CALL    I2C_Start               ; Send start, control byte and ack
+
+                CALL    I2C_Recv                ; Read 8 bits of data and send ack
+                CALL    I2C_MNAck
+                INPUT    s2, sC
+
+                JUMP    I2C_Stop                ; Send Stop
+
+; -------------------------------------------------------------------------------
+; Routine to set up for and write one byte to I2C device
+; -------------------------------------------------------------------------------
+Write1_I2C:
+
+                CALL    I2C_Start               ; Send Start, control byte and ack
+                INPUT    sC, s0                  ; Load device code for TX
+                SL0     sC                      ; add write flag
+                CALL    I2C_Xmit                ; Send address and ack
+                CALL    I2C_SAck
+
+                INPUT    sC, s1                  ; Load data for TX
+                CALL    I2C_Xmit                ; Send data and ack
+                CALL    I2C_SAck
+
+                JUMP    I2C_Stop                ; Send stop
+
+; -------------------------------------------------------------------------------
+; Transmit 8 bits of I2C data
+; -------------------------------------------------------------------------------
+I2C_Xmit:
+                INPUT    sD, 8                   ; Load I2C bit counter
+I2C_TX:
+                CALL    Delay
+                SL0     sC                      ; Move data bit -> carry
+                JUMP    C, I2C_TX1              ; Jump if bit high
+I2C_TX0:
+                OUTPUT     sF, bSDA_0              ; SDA = 0
+                CALL    Delay
+                JUMP    I2C_TX2                 ; Jump over next instructions
+I2C_TX1:
+                OUTPUT     sF, bSDA_Z              ; SDA = 1 due to pull-up
+                CALL    Delay
+I2C_TX2:
+
+                OUTPUT     sF, bSCL_Z              ; SCL = 1 due to pull-up
+                CALL    Delay
+                CALL    Delay
+                CALL    Delay
+                OUTPUT     sF, bSCL_0              ; SCL = 0
+
+                SUB     sD, 1                   ; Decrement I2C bit counter
+                JUMP    NZ, I2C_TX              ; Loop until 8 bits are sent
+                JUMP    Delay
+
+; -------------------------------------------------------------------------------
+; Read 8 bits of I2C data
+; -------------------------------------------------------------------------------
+I2C_Recv:
+                INPUT    sD, 8                   ; Load I2C bit counter
+                CALL    Delay
+                OUTPUT     sF, bSDA_Z              ; SDA = 1 due to pull-up
+
+I2C_RX:
+                CALL    Delay
+                OUTPUT     sF, bSCL_Z              ; SCL = 1 due to pull-up
+                CALL    Delay
+                CALL    Delay
+                IN      sF, bSDA_In
+                CALL    Delay
+                SR0     sF                      ; LSBit -> carry bit
+                SLA     sC                      ; Carry shifted into LSBit
+                OUTPUT     sF, bSCL_0              ; SCL = 0
+                CALL    Delay
+                SUB     sD, 1                   ; Decrement I2C bit counter
+                JUMP    NZ, I2C_RX              ; Loop until 8 bits are read
+                RETURN
+
+; -------------------------------------------------------------------------------
+; Ack by master, keep data 0
+; -------------------------------------------------------------------------------
+I2C_MAck:
+                CALL    Delay
+                OUTPUT     sF, bSDA_0              ; SDA = 0
+                CALL    Delay
+                OUTPUT     sF, bSCL_Z              ; SCL = 1 due to pull-up
+                CALL    Delay
+                CALL    Delay
+                CALL    Delay
+                OUTPUT     sF, bSCL_0              ; SCL = 0
+                CALL    Delay
+                OUTPUT     sF, bSDA_0              ; SDA = 0
+                RETURN
+
+; -------------------------------------------------------------------------------
+; NAck by master, release data
+; -------------------------------------------------------------------------------
+I2C_MNAck:
+                CALL    Delay
+                OUTPUT     sF, bSDA_Z              ; SDA = 1 due to pull-up
+                CALL    Delay
+                OUTPUT     sF, bSCL_Z              ; SCL = 1 due to pull-up
+                CALL    Delay
+                CALL    Delay
+                CALL    Delay
+                OUTPUT     sF, bSCL_0              ; SCL = 0
+                CALL    Delay
+                OUTPUT     sF, bSDA_0              ; SDA = 0
+                RETURN
+
+; -------------------------------------------------------------------------------
+; Ack by slave, release data
+; -------------------------------------------------------------------------------
+I2C_SAck:
+                CALL    Delay
+                OUTPUT     sF, bSDA_Z              ; SDA = 1 due to pull-up
+                CALL    Delay
+                OUTPUT     sF, bSCL_Z              ; SCL = 1 due to pull-up
+                CALL    Delay
+                CALL    Delay
+                CALL    Delay
+                OUTPUT     sF, bSCL_0              ; SCL = 0
+                CALL    Delay
+                RETURN
+
+; -------------------------------------------------------------------------------
+; Set up start condition for I2C
+; -------------------------------------------------------------------------------
+
+; SCL and SDA set to inputs, signals go high due to pull-up resistors
+
+I2C_RepStart:
+                CALL    Delay
+                OUTPUT     sF, bSCL_Z              ; SCL = Z
+I2C_Start:
+                CALL    Delay
+                OUTPUT     sF, bSDA_0              ; SDA = 0
+                CALL    Delay
+                OUTPUT     sF, bSCL_0              ; SCL = 0
+                CALL    Delay
+                RETURN
+
+; -------------------------------------------------------------------------------
+; Send I2C stop command
+; -------------------------------------------------------------------------------
+I2C_Stop:
+                CALL    Delay
+                OUTPUT     sF, bSCL_Z              ; SCL = 1 due to pull-up
+                CALL    Delay
+                OUTPUT     sF, bSDA_Z              ; SDA = 1 due to pull-up
+                CALL    Delay
+                RETURN
+
+; -----------I2C Subroutines end-------------------------------------------------
