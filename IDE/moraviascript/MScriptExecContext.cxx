502cc2796cba7362548bc55bbccbcd7ed3c54d02
Martin Ošmera <martin.osmera@gmail.com>
2013-06-07 13:31:40 +0200

    .

---
+++ IDE/moraviascript/MScriptExecContext.cxx
@@ -0,0 +1,67 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup MoraviaScript
+ * @file MScriptExecContext.cxx
+ */
+// =============================================================================
+
+#include "MScriptExecContext.h"
+
+void MScriptExecContext::init ( const MScriptStatement * rootNode )
+{
+    clear();
+    m_programPointer.push_back(ProgPtr(rootNode, FLAG_NORMAL));
+}
+
+void MScriptExecContext::clear()
+{
+    m_programPointer.clear();
+}
+
+void MScriptExecContext::popNext()
+{
+    m_programPointer.pop_back();
+}
+
+const MScriptStatement * MScriptExecContext::getNextNode() const
+{
+    return m_programPointer.back().first;
+}
+
+MScriptExecContext::ExecFlags MScriptExecContext::getNextFlags() const
+{
+    return m_programPointer.back().second;
+}
+
+void MScriptExecContext::setNext ( const MScriptStatement * node,
+                                   MScriptExecContext::ExecFlags flags )
+{
+    if ( NULL != node )
+    {
+        m_programPointer.push_back(ProgPtr(node, flags));
+    }
+}
+
+void MScriptExecContext::setNextFlags ( MScriptExecContext::ExecFlags flags )
+{
+    if ( false == m_programPointer.empty() )
+    {
+        m_programPointer.back().second = flags;
+    }
+}
+
+void MScriptExecContext::replaceNext ( const MScriptStatement * node,
+                                       MScriptExecContext::ExecFlags flags )
+{
+    popNext();
+    setNext(node, flags);
+}
+
---
+++ IDE/moraviascript/MScriptExecContext.h
@@ -0,0 +1,131 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Interface: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup MoraviaScript
+ * @file MScriptExecContext.h
+ */
+// =============================================================================
+
+#ifndef MSCRIPTEXECCONTEXT_H
+#define MSCRIPTEXECCONTEXT_H
+
+// Forward declarations.
+class MScriptStatement;
+
+// Standard header files.
+#include <cstddef>
+#include <vector>
+#include <utility>
+
+/**
+ * @brief
+ * @class MScriptExecContext
+ * @ingroup MoraviaScript
+ */
+class MScriptExecContext
+{
+    ////    Public Datatypes    ////
+    public:
+        /**
+         * @brief
+         */
+        enum ExecFlags
+        {
+            FLAG_LOOP   = 0x01, ///<
+            FLAG_SWITCH = 0x02, ///<
+            FLAG_CALL   = 0x04, ///<
+
+            FLAG_NORMAL = 0x00  ///<
+        };
+
+        /**
+         * @brief
+         */
+        typedef std::pair<const MScriptStatement*, ExecFlags> ProgPtr;
+
+    ////    Public Operations    ////
+    public:
+        /**
+         * @brief
+         * @param[in,out] rootNode
+         */
+        void init ( const MScriptStatement * rootNode );
+
+        /**
+         * @brief
+         */
+        void clear();
+
+        /**
+         * @brief
+         * @param[in] node
+         * @param[in] flags
+         */
+        void setNext ( const MScriptStatement * node,
+                       ExecFlags flags = FLAG_NORMAL );
+
+        /**
+         * @brief
+         * @param[in] flags
+         */
+        void setNextFlags ( ExecFlags flags );
+
+        /**
+         * @brief
+         * @param[in] node
+         * @param[in] flags
+         */
+        void replaceNext ( const MScriptStatement * node,
+                           ExecFlags flags = FLAG_NORMAL );
+
+        /**
+         * @brief
+         */
+        void popNext();
+
+        /**
+         * @brief
+         * @return
+         */
+        ExecFlags getNextFlags() const;
+
+        /**
+         * @brief
+         * @return
+         */
+        const MScriptStatement * getNextNode() const;
+
+    ////    Inline Public Operations    ////
+    public:
+        /**
+         * @brief
+         * @return
+         */
+        bool empty() const
+        {
+            return m_programPointer.empty();
+        }
+
+        /**
+         * @brief
+         * @return
+         */
+        std::vector<ProgPtr> & getProgramPointer()
+        {
+            return m_programPointer;
+        }
+
+    ////    Private Attributes    ////
+    private:
+        /// @brief
+        std::vector<ProgPtr> m_programPointer;
+};
+
+#endif // MSCRIPTEXECCONTEXT_H
