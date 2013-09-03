95a03439dd38124f4d48e9c5a0a66eb49dba90cd
Martin Ošmera <martin.osmera@gmail.com>
2013-09-03 12:24:47 +0200

    .

--- IDE/moraviascript/MScriptVarTable.h
+++ IDE/moraviascript/MScriptVarTable.h
@@ -16,6 +16,15 @@
 #ifndef MSCRIPTVARTABLE_H
 #define MSCRIPTVARTABLE_H
 
+// Forward declarations.
+class MScriptValue;
+
+// Standard header files.
+#include <map>
+#include <vector>
+#include <string>
+#include <ostream>
+
 /**
  * @brief
  * @ingroup MoraviaScript
@@ -23,6 +32,45 @@
  */
 class MScriptVarTable
 {
+    ////    Private Datatypes    ////
+    private:
+        /**
+         * @brief
+         */
+        enum Flags
+        {
+            FLAG_CONST = 0x01, ///< 0 == variable, 1 == constant.
+            FLAG_ARRAY = 0x02, ///< 0 == scalar, 1 == array.
+            FLAG_HASH  = 0x04, ///< 0 == indexed array, 1 == hash - associative array.
+        };
+
+        /**
+         * @brief
+         */
+        union Value
+        {
+            MScriptValue * m_scalar;
+            std::vector<MScriptValue*> * m_array;
+            std::map<std::string,MScriptValue*> * m_hash;
+        };
+
+        /**
+         * @brief
+         */
+        struct Variable
+        {
+            /// @brief
+            Flags m_type;
+
+            /// @brief
+            Value m_value;
+        };
+
+        /**
+         * @brief
+         */
+        typedef std::map<std::string,Variable> VarTable;
+
     ////    Public Operations    ////
     public:
         /**
@@ -41,6 +89,23 @@ class MScriptVarTable
          * @return
          */
         bool remove ( const std::string & variable );
+
+    ////    Private Attributes    ////
+    private:
+        /// @brief
+        std::vector<VarTable> m_varTables;
 };
+
+/// @name Tracing operators
+//@{
+    /**
+     * @brief
+     * @param[in,out] out
+     * @param[in] table
+     * @return
+     */
+    std::ostream & operator << ( std::ostream & out,
+                                 const MScriptVarTable & table );
+//@}
 
 #endif // MSCRIPTVARTABLE_H
