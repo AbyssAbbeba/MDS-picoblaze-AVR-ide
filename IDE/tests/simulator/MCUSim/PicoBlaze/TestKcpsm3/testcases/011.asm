a415ed2bcd4d9050227b8d0ad48d389a3ae8596b
Martin Madron <pomerange@seznam.cz>
2013-09-04 14:50:50 +0200

    .

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/011.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/011.asm
@@ -9,34 +9,19 @@ Temp2         REG       s1
 Temp3         REG       s2
 
 ; Begining of main loop
-Start:              CALL      wait_1s
-                    CALL      wait_100ms
-                    JUMP      $                   ; Infinite loop, $ is translated as number of curent line
-                    ;END                           ; End of program
+Start:          CALL      pod1
+                            ; End of program
 
+pod2:           CALL    POD3
+                RET
 
-; Subroutines-----------------------------------------------------------------
-; wait_time = (4 + (((2 * Temp1) + 2) * Temp2 + 2) * Temp3) * 2 * clk_period
-;   1s @ (10 MHz, Temp1 = 250, Temp2 = 249, Temp3 = 40)
+pod3:           CALL    POD4
+                RET
 
-wait_1s:            LOAD      Temp1, #250          ; Load Temp1 register
-                    LOAD      Temp2, #249          ; Load Temp2 register
-                    LOAD      Temp3, #200          ; Load Temp3 register
-wait_1s_i:          SUB       Temp1, 1
-                    JUMP      NZ, wait_1s_i
-                    SUB       Temp2, 1
-                    JUMP      NZ, wait_1s_i
-                    SUB       Temp3, 1
-                    JUMP      NZ, wait_1s_i
-                    RETURN
-;-----------------------------------------------------------------------------
-wait_100ms:         LOAD      Temp1, #250          ; Load Temp1 register
-                    LOAD      Temp2, #249          ; Load Temp2 register
-                    LOAD      Temp3, #20           ; Load Temp3 register
-wait_100ms_i:       SUB       Temp1, 1
-                    JUMP      NZ, wait_100ms_i
-                    SUB       Temp2, 1
-                    JUMP      NZ, wait_100ms_i
-                    SUB       Temp3, 1
-                    JUMP      NZ, wait_100ms_i
-                    RET
+pod4:           CALL    POD5
+                RET
+
+pod5:           CALL    POD6
+                RET
+
+pod6:           RET
