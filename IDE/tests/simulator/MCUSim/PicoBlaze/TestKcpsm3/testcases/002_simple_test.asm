014d0faf8104cf4d588bd5887b83c532165fbab3
Abyss <abyss@abyss-K52JT.(none)>
2013-09-10 13:13:01 +0200

    .

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/002_simple_test.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/002_simple_test.asm
@@ -3,9 +3,6 @@
    DEVICE      KCPSM3
 
     ORG         0x00
-    JUMP        Start
-    
-
 
 Start:
         LOAD    0x00,#1
@@ -14,4 +11,17 @@ Start:
 
 HOP1:   JUMP    HOP2
 HOP2:   JUMP    HOP3
-HOP3:   END
+HOP3:   JUMP    HOP4
+        LOAD    s0,#1
+HOP4:   JUMP    HOP5
+HOP5:   JUMP    HOP6
+        LOAD    s0,#1
+        LOAD    s0,#1
+        LOAD    s0,#1
+HOP6:   JUMP    HOP7
+HOP7:   JUMP    HOP8
+        LOAD    s0,#1
+        LOAD    s0,#12
+HOP8:   
+
+END
