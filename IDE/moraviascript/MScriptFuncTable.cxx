193b02b4b638fd72d5c509255f8cef92f7503ad9
Martin Ošmera <martin.osmera@gmail.com>
2013-09-11 16:37:38 +0200

    .

--- IDE/moraviascript/MScriptFuncTable.cxx
+++ IDE/moraviascript/MScriptFuncTable.cxx
@@ -23,6 +23,28 @@
 // Used for i18n only
 #include <QObject>
 
+
+MScriptFuncTable::Parameter::Parameter ( const char * name,
+                                         const MScriptValue * defaulValue )
+                                       :
+                                         m_name ( name )
+{
+    if ( NULL == defaulValue )
+    {
+        m_defaulValue = NULL;
+    }
+    else
+    {
+        m_defaulValue = new MScriptValue();
+        *m_defaulValue = defaulValue->makeCopy();
+    }
+}
+
+MScriptFuncTable::Parameter::~Parameter()
+{
+    delete m_defaulValue;
+}
+
 MScriptFuncTable::Function::Function()
 {
     m_code   = NULL;
@@ -37,8 +59,8 @@ MScriptFuncTable::Function::~Function()
     }
     if ( NULL != m_params )
     {
-        m_params->completeDelete();
-    };
+        delete m_params;
+    }
 }
 
 bool MScriptFuncTable::define ( const std::string & name,
