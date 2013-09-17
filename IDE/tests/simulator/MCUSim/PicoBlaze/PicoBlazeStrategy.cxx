3ae33263078e18fe867c7ee11901e2175e38243b
Martin Ošmera <martin.osmera@gmail.com>
2013-09-17 16:21:29 +0200

    .

--- IDE/tests/simulator/MCUSim/PicoBlaze/PicoBlazeStrategy.cxx
+++ IDE/tests/simulator/MCUSim/PicoBlaze/PicoBlazeStrategy.cxx
@@ -15,15 +15,128 @@
 
 #include "PicoBlazeStrategy.h"
 
-bool PicoBlazeStrategy::processLine ( const char * line,
+#include "../../../../simulators/MCUSim/MCUSim.h"
+#include "../../../../simulators/MCUSim/PicoBlaze/PicoBlazeStatusFlagsBase.h"
+
+bool PicoBlazeStrategy::processLine ( std::vector<std::string> * tokens,
                                       bool useAsmFile,
                                       MCUSimTestScript::Command * cmd )
 {
-    
+    switch ( tokens->size() )
+    {
+        case 7:
+            if (
+                   ( "[" == tokens->at(1) )
+                       &&
+                   ( "]" == tokens->at(3) )
+                       &&
+                   ( "=" == tokens->at(5) )
+                       &&
+                   ( true == MCUSimTestScript::checkBool(tokens->at(6)) )
+               )
+            {
+                if ( "=" == tokens->at(4) )
+                {
+                    cmd->m_type = CTE_FLAG_EQ;
+                }
+                else if ( "!" == tokens->at(4) )
+                {
+                    cmd->m_type = CTE_FLAG_NE;
+                }
+                else
+                {
+                    break;
+                }
+
+                if ( 0 != strcmp("FLAG", tokens->at(0).c_str()) )
+                {
+                    break;
+                }
+
+                std::transform(tokens->at(2).begin(), tokens->at(2).end(), tokens->at(2).begin(), ::toupper);
+
+                int flagID;
+                if ( 0 != strcmp("Z", tokens->at(2).c_str()) )
+                {
+                    flagID = FID_Z;
+                }
+                else if ( 0 != strcmp("C", tokens->at(2).c_str()) )
+                {
+                    flagID = FID_C;
+                }
+                else if ( 0 != strcmp("PZ", tokens->at(2).c_str()) )
+                {
+                    flagID = FID_PZ;
+                }
+                else if ( 0 != strcmp("PC", tokens->at(2).c_str()) )
+                {
+                    flagID = FID_PC;
+                }
+                else if ( 0 != strcmp("IE", tokens->at(2).c_str()) )
+                {
+                    flagID = FID_IE;
+                }
+                else if ( 0 != strcmp("I", tokens->at(2).c_str()) )
+                {
+                    flagID = FID_I;
+                }
+                else
+                {
+                    *m_execMessage = "flag not recognized";
+                    cmd->m_type = MCUSimTestScript::CT_ABORT;
+                    break;
+                }
+
+                cmd->m_args.push_back(flagID);
+                cmd->m_args.push_back(MCUSimTestScript::str2bool(tokens->at(6)));
+                return true;
+            }
+            break;
+    }
+
+    return false;
 }
 
 bool PicoBlazeStrategy::executeCommand ( const MCUSimTestScript::Command & cmd,
                                          std::ofstream & outFile )
 {
-    
+    switch ( cmd.m_type )
+    {
+        case CTE_FLAG_EQ:
+        case CTE_FLAG_NE:
+        {
+            int flagValue;
+            PicoBlazeStatusFlagsBase * flags = dynamic_cast<PicoBlazeStatusFlagsBase*>
+                                               (m_simulator->getSubsys(MCUSimSubsys::ID_FLAGS));
+
+            switch ( cmd.m_args[0] )
+            {
+                case FID_Z:  flagValue = ( ( true == flags->m_zero     ) ? 1 : 0 ); break;
+                case FID_C:  flagValue = ( ( true == flags->m_carry    ) ? 1 : 0 ); break;
+                case FID_PZ: flagValue = ( ( true == flags->m_preZero  ) ? 1 : 0 ); break;
+                case FID_PC: flagValue = ( ( true == flags->m_preCarry ) ? 1 : 0 ); break;
+                case FID_IE: flagValue = ( ( true == flags->m_inte     ) ? 1 : 0 ); break;
+                case FID_I:  flagValue = ( ( flags->m_interrupted > 0  ) ? 1 : 0 ); break;
+
+                default:
+                    return false;
+            }
+
+            if ( cmd.m_args[1] == flagValue )
+            {
+                if ( CTE_FLAG_EQ != cmd.m_type )
+                {
+                    m_success = false;
+                }
+            }
+            else if ( CTE_FLAG_NE != cmd.m_type )
+            {
+                m_success = false;
+            }
+
+            return true;
+        }
+    }
+
+    return false;
 }
