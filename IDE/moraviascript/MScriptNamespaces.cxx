2e66bd6212e0f8414285764a487b2e934deff043
Martin Ošmera <martin.osmera@gmail.com>
2013-10-22 15:47:50 +0200

    .

--- IDE/moraviascript/MScriptNamespaces.cxx
+++ IDE/moraviascript/MScriptNamespaces.cxx
@@ -56,7 +56,7 @@ bool MScriptNamespaces::NsDesc::constains ( const NsDesc * ns ) const
           it != m_contains.cend();
           it++ )
     {
-        if ( true == it->constains(ns) )
+        if ( true == (*it)->constains(ns) )
         {
             return true;
         }
@@ -73,43 +73,133 @@ void MScriptNamespaces::leave()
 void MScriptNamespaces::define ( const MScriptSrcLocation & location,
                                  const std::string & ns )
 {
+//     current()
+// 
+//     m_ns
 }
 
 void MScriptNamespaces::enter ( const MScriptSrcLocation & location,
-                                const std::string & ns )
+                                MScriptNamespaces::NsDesc * ns )
 {
+    return m_current.push_back(ns);
 }
+
 void MScriptNamespaces::defineEnter ( const MScriptSrcLocation & location,
                                       const std::string & ns )
 {
     
 }
 
-const MScriptNamespaces::NsDesc * MScriptNamespaces::current() const
+MScriptNamespaces::NsDesc * MScriptNamespaces::current() const
 {
     return m_current.back();
 }
 
 void MScriptNamespaces::clear()
 {
-    m_current.clear();
     if ( NULL != m_ns )
     {
         delete m_ns;
     }
+    m_ns = new NsDesc ( "::", NULL, MScriptSrcLocation() );
 
-    m_ns = new NsDesc ( "::", NULL );
+    m_current.clear();
     m_current.push_back(m_ns);
+}
+
+
+void MScriptNamespaces::NsDesc::print ( std::ostream & out,
+                                        int level,
+                                        std::string lineString ) const
+{
+    char addr[19];
+    sprintf ( addr,
+              "0x%08x%08x",
+              (unsigned int) ( ( 0xffffffff00000000 & reinterpret_cast<long long unsigned int>(this) ) >> 32),
+              (unsigned int) (   0x00000000ffffffff & reinterpret_cast<long long unsigned int>(this) ) );
+    out << addr << " ";
+
+    if ( NULL == this )
+    {
+        out << "<ERROR:NULL!>" << std::endl;
+        return;
+    }
+
+    for ( int i = 0; i < level; i++ )
+    {
+        if ( '0' == lineString[i] )
+        {
+            out << "    ";
+        }
+        else
+        {
+            out << "  │ ";
+        }
+    }
+
+    if ( NULL == m_parent )
+    {
+        if ( true == m_contains.empty() )
+        {
+            lineString[level] = '0';
+        }
+        out << "  █─ ";
+    }
+    else
+    {
+        if ( true == m_contains.empty() )
+        {
+            out << "  └─ ";
+            lineString[level] = '0';
+        }
+        else
+        {
+            out << "  ├─ ";
+        }
+    }
+
+    out << m_name;
+
+    out << " {";
+    out << m_location;
+    out << "}";
+    out << std::endl;
+
+    level++;
+    lineString += "1";
+    for ( std::vector<NsDesc*>::const_iterator i = m_contains.cbegin();
+          i != m_contains.cend();
+          i++ )
+    {
+        (*i)->print(out, level, lineString);
+    }
 }
 
 std::ostream & operator << ( std::ostream & out,
                              const MScriptNamespaces & namespaces )
 {
+    namespaces.m_ns->print(out);
     return out;
 }
 
 std::ostream & operator << ( std::ostream & out,
                              const MScriptNamespaces::NsDesc * nsDesc )
 {
+    std::vector<const std::string*> path;
+
+    for ( const MScriptNamespaces::NsDesc * ns = nsDesc;
+          ns != NULL;
+          ns = ns->m_parent )
+    {
+        path.push_back ( &(ns->m_name) );
+    }
+
+    for ( std::vector<const std::string*>::const_reverse_iterator i = path.crbegin();
+          i != path.crend();
+          i++ )
+    {
+        out << "::" << **i;
+    }
+
     return out;
 }
