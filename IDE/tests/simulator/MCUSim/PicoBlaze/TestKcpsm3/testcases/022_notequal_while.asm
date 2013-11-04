085e2278ab118915197d75a75b42d9e114132988
Abyss <abyss@abyss-K52JT.(none)>
2013-11-04 14:29:14 +0100

    .

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/022_notequal_while.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/022_notequal_while.asm
@@ -9,22 +9,73 @@ start:
         load    s1,#9
         ;; step 3
         ;; reg[0] == 10
-        ;; reg[1] == 8
+        ;; reg[1] == 9
         
         while   s1  != #10
             add     s1,#1
         endw
 
+        sub     s1,#1
         jump    next1
-        ;; step
-        ;; pc == 3
+        ;; step 2
         ;; flag[z] == false
         ;; flag[c] == true
-        ;; step 3
+        ;; step 4
         ;; reg[1] == 10
         ;; flag[z] == true
         ;; flag[c] == false
         ;; step
-        ;; pc == 6
+
 next1:
+
+        while   1  != #10
+            add     s1,#1
+        endw
+
+        sub     s1,#1
+        jump    next2
+        ;; step 2
+        ;; flag[z] == false
+        ;; flag[c] == true
+        ;; step 4
+        ;; reg[1] == 10
+        ;; flag[z] == true
+        ;; flag[c] == false
+        ;; step
+
+next2:
+
+        while   s1  != s0
+            add     s1,#1
+        endw
+
+        sub     s1,#1
+        jump    next3
+        ;; step 2
+        ;; flag[z] == false
+        ;; flag[c] == true
+        ;; step 4
+        ;; reg[1] == 10
+        ;; flag[z] == true
+        ;; flag[c] == false
+        ;; step
+
+next3:
+
+        while   1  != 0
+            add     s1,#1
+        endw
+
+        sub     s1,#1
+        jump    next4
+        ;; step 2
+        ;; flag[z] == false
+        ;; flag[c] == true
+        ;; step 4
+        ;; reg[1] == 10
+        ;; flag[z] == true
+        ;; flag[c] == false
+        ;; step
+
+next4:
 
