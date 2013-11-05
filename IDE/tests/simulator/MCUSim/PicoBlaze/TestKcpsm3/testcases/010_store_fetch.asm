16687e6b54d53ba043215422fd87c4f0fe3c300d
Abyss <abyss@abyss-K52JT.(none)>
2013-11-05 16:32:51 +0100

    .

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
@@ -1,13 +1,20 @@
-        org     0
-       
-
-start:
-
-		        FOR     s0,2
-            FOR         s0,1
-                add         s9,#1
-            ENDF
-            add     sA,#1
-        ENDF
-
-		jump start
+      ;   FOR     s0,2
+      LOAD    0x0, #0x0
+FOR_3:
+      COMPARE 0x0, #0x2
+      JUMP    Z, FOR_3_END
+                                 ; FOR         s0,1
+      LOAD    0x0, #0x0
+FOR_4:
+     COMPARE 0x0, #0x1
+     JUMP    Z, FOR_4_END
+        add         s9,#1
+                                ;  ENDF
+      ADD     0x0, #0x1
+      JUMP    FOR_4
+FOR_4_END:
+         add     sA,#1
+                          ;   ENDF
+      ADD     0x0, #0x1
+     JUMP    FOR_3
+       FOR_3_END:
