2aa393c1c4ec2e69214c97f6bc55f0465c41d172
Abyss <abyss@abyss-K52JT.(none)>
2013-11-13 16:42:18 +0100

    .

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/011_ANd_or_xor.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/011_ANd_or_xor.asm
@@ -13,14 +13,12 @@ Start:
     LOAD        s3,#8
     ;;STEP 4
     ;; REG[0] == 1
-    ;;STEP
-    ;;REG[0] == 0
 
     AND         s0,#1
     AND         s0,s1
 
     ;;STEP
-    ;; REG[0]==2
+    ;; REG[0]==1
     ;;STEP
     ;;REG[1] == 2
 
@@ -28,9 +26,16 @@ Start:
     OR          s1,s1
 
     ;;STEP
-    ;;REG[2] == 6
+    ;;REG[0] == 2
     ;;STEP
-    ;; REG[3] == 10
+    ;; REG[1] == 2
 
     XOR         s2,s1
-    XOR         s3,s1   
+    XOR         s3,s1
+    ;; step
+    ;; reg[2] == 6
+    ;; step
+    ;; reg[3] == 10
+
+
+    
