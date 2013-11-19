9684d7b70c59dfe45319dd8e40c9f387717e1951
Abyss <abyss@abyss-K52JT.(none)>
2013-11-19 17:32:46 +0100

    .

---
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm2/testcases/017_interrupts.asm
@@ -0,0 +1,41 @@
+; TEST CASE
+; testing interrupts behavior
+device kcpsm2
+org     0
+
+start:
+
+        ;;      flag[ie] == false
+        ;;      step
+        ;;      reg[0] == 10
+        ;;      interrupt
+        ;;      flag[ie] == false
+        ;;       pc == 1
+        LOAD    S0, #10
+        ;; step 2
+        ;; pc == 3
+        ;; flag[ie] == true
+        ENABLE INTERRUPT  
+        ;; step
+        ;; reg[0] == 249
+WAIT:   LOAD    s0,#250
+        ;; step
+        ;; flag[ie] == true
+        ;; interrupt
+        SUB     s0,#1
+        jump    NZ,WAIT
+         DISABLE INTERRUPT
+        
+INTERRUPT:
+            ;; step
+            ;; pc == 1023
+            
+            IN          S2,0
+            OUT         S2,0
+            RETURNI ENABLE
+
+org     3ffh
+jump    INTERRUPT
+
+            
+        END
