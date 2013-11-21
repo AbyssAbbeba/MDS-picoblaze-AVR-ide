f78a78c1ab8770d1d332a044ceb82d3aa01923e4
Martin Ošmera <martin.osmera@gmail.com>
2013-11-21 14:39:57 +0100

    .

---
+++ IDE/utilities/AsmTranslator/AsmTranslator.h
@@ -0,0 +1,28 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Interface: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup AsmTranslator
+ * @file AsmTranslator.h
+ */
+// =============================================================================
+
+#ifndef ASMTRANSLATOR_H
+#define ASMTRANSLATOR_H
+
+/**
+ * @brief
+ * @ingroup AsmTranslator
+ * @class AsmTranslator
+ */
+class AsmTranslator
+{
+};
+
+#endif // ASMTRANSLATOR_H
