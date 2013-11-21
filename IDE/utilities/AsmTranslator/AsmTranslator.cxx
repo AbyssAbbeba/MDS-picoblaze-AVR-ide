934bf258568be1ffd392a45ce5004223d03f5626
Martin Ošmera <martin.osmera@gmail.com>
2013-11-21 14:39:57 +0100

    .

---
+++ IDE/utilities/AsmTranslator/AsmTranslator.cxx
@@ -0,0 +1,16 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup AsmTranslator
+ * @file AsmTranslator.cxx
+ */
+// =============================================================================
+
+#include "AsmTranslator.h"
