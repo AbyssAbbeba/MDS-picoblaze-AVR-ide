872fd2f741bedbd1e72a939fc3563a8dceff2af3
Martin Ošmera <martin.osmera@gmail.com>
2013-11-25 15:15:57 +0100

    .

--- IDE/utilities/AsmTranslator/AsmTranslator.h
+++ IDE/utilities/AsmTranslator/AsmTranslator.h
@@ -16,6 +16,10 @@
 #ifndef ASMTRANSLATOR_H
 #define ASMTRANSLATOR_H
 
+// Standard header files.
+#include <string>
+#include <vector>
+
 /**
  * @brief
  * @ingroup AsmTranslator
@@ -23,6 +27,63 @@
  */
 class AsmTranslator
 {
+    ////    Public Datatypes    ////
+    public:
+        /**
+         * @brief
+         */
+        enum Variant
+        {
+            V_KCPSM_XILINX
+        };
+
+    ////    Constructors and Destructors    ////
+    public:
+        /**
+         * @brief
+         */
+        virtual ~AsmTranslator();
+
+    ////    Public Operations    ////
+    public:
+        /**
+         * @brief
+         * @param[in] variant
+         * @param[in,out] output
+         * @param[in,out] input
+         */
+        bool translate ( Variant variant,
+                         std::ostream & output,
+                         std::istream & input );
+
+        /**
+         * @brief
+         * @param[in] variant
+         * @param[in,out] output
+         * @param[in,out] input
+         * @param[in] makeBackup
+         * @return
+         */
+        bool translate ( Variant variant,
+                         const std::string & output,
+                         const std::string & input,
+                         bool makeBackup = false );
+
+        /**
+         * @brief
+         * @return
+         */
+        const std::vector<std::string> & getMessages() const;
+
+        /**
+         * @brief
+         */
+        void clear();
+
+    ////    Private Attributes    ////
+    private:
+        /// @brief
+        std::vector<std::string> m_messages;
 };
 
 #endif // ASMTRANSLATOR_H
