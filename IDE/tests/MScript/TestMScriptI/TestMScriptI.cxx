e439fedbb59da6f2273c93bc03c534367a954e03
Martin Ošmera <martin.osmera@gmail.com>
2013-12-02 15:50:17 +0100

    .

---
+++ IDE/tests/MScript/TestMScriptI/TestMScriptI.cxx
@@ -0,0 +1,32 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup testsMScript
+ * @file TestMScriptI.cxx
+ */
+// =============================================================================
+
+#include "TestMScriptI.h"
+
+int TestMScriptI::init()
+{
+}
+
+int TestMScriptI::clean()
+{
+}
+
+bool TestMScriptI::addTests ( CU_pSuite suite )
+{
+}
+
+void TestMScriptI::testFunction()
+{
+}
---
+++ IDE/tests/MScript/TestMScriptIII/TestMScriptIII.h
@@ -0,0 +1,64 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Interface: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup testsMScript
+ * @file TestMScriptIII.h
+ */
+// =============================================================================
+
+#ifndef TESTMSCRIPTIII_H
+#define TESTMSCRIPTIII_H
+
+// Forward declarations.
+class MScript;
+
+// The CUnit testing framework.
+#include <CUnit/Basic.h>
+
+/**
+ * @brief
+ * @namespace TestMScriptIII
+ * @ingroup testsMScript
+ */
+namespace TestMScriptIII
+{
+    ////    Operations    ////
+        /**
+         * @brief
+         * @return
+         */
+        int init();
+
+        /**
+         * @brief
+         * @return
+         */
+        int clean();
+
+        /**
+         * @brief
+         * @param[in,out] suite
+         * @return
+         */
+        bool addTests ( CU_pSuite suite );
+
+        /**
+         * @brief
+         */
+        void testFunction();
+
+    ////    Attributes    ////
+        /**
+         * @brief
+         */
+        MScript * m_core;
+};
+
+#endif // TESTMSCRIPTIII_H
