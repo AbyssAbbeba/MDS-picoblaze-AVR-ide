13ea81703a8569a2dbf2328c1cb3fd0919f56089
Martin Ošmera <martin.osmera@gmail.com>
2013-12-02 15:50:17 +0100

    .

---
+++ IDE/tests/MScript/TestMScriptII/TestMScriptII.cxx
@@ -0,0 +1,32 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup testsMScript
+ * @file TestMScriptII.cxx
+ */
+// =============================================================================
+
+#include "TestMScriptII.h"
+
+int TestMScriptII::init()
+{
+}
+
+int TestMScriptII::clean()
+{
+}
+
+bool TestMScriptII::addTests ( CU_pSuite suite )
+{
+}
+
+void TestMScriptII::testFunction()
+{
+}
