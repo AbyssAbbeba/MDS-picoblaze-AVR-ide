4565580dcdc9f31392488c81e263c3fd1ddc5f86
Erik Chalupa <erik.chalupa@gmail.com>
2013-12-06 15:58:18 +0100

    +(GUI) InterfaceCfg syntax update

--- IDE/GUI/dialogs/interfacecfg/interfacecfg_syntaxasm.h
+++ IDE/GUI/dialogs/interfacecfg/interfacecfg_syntaxasm.h
@@ -2,18 +2,37 @@
 #define INTERFACECFG_SYNTAXASM_H
 
 #include <QWidget>
+#include <QColor>
 #include "ui_interfacecfg_syntaxasm.h"
 
 
 class InterfaceCfg_SyntaxAsm : public QWidget
 {
     Q_OBJECT
+    
     public:
         InterfaceCfg_SyntaxAsm(QWidget *parent);
 
+        typedef enum
+        {
+            COMMENT = 0,
+            LABEL,
+            KEYWORD,
+            OPERAND,
+            OPERATOR,
+            QUOTE
+        } Types;
+
     private:
-        int type;
+        Types type;
         Ui_InterfaceCfg_SyntaxAsm ui;
+
+    signals:
+        void colorChanged(const QColor &color, InterfaceCfg_SyntaxAsm::Types type);
+        void italicChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
+        void underlineChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
+        void strikeChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
+        void boldChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
 
     private slots:
         void commentColor();
