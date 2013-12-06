ee1e8f5ccce6419825063291e57cfac46daf78cc
Erik C <xchalu10@stud.fit.vutbr.cz>
2013-12-06 15:58:18 +0100

    +(GUI) InterfaceCfg syntax update

--- IDE/GUI/dialogs/interfacecfg/interfacecfg_editsyntax.h
+++ IDE/GUI/dialogs/interfacecfg/interfacecfg_editsyntax.h
@@ -2,6 +2,8 @@
 #define INTERFACECFG_EDITSYNTAX_H
 
 #include <QWidget>
+#include <QColor>
+#include "interfacecfg_syntaxasm.h"
 #include "ui_interfacecfg_editsyntax.h"
 
 
@@ -13,6 +15,13 @@ class InterfaceCfg_EditSyntax : public QWidget
 
     private:
         Ui_InterfaceCfg_EditSyntax ui;
+
+    private slots:
+        void colorChanged(const QColor &color, InterfaceCfg_SyntaxAsm::Types type);
+        void italicChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
+        void underlineChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
+        void strikeChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
+        void boldChanged(bool enabled, InterfaceCfg_SyntaxAsm::Types type);
 
 };
 
