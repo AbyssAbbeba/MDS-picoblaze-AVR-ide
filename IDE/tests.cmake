fc3ba229c04278c0249655e367995ddacfa12ff6
Martin Ošmera <martin.osmera@gmail.com>
2014-01-03 12:28:54 +0100

    .

---
+++ IDE/tests.cmake
@@ -0,0 +1,30 @@
+# ==============================================================================
+#
+# (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
+#
+# author: Martin Ošmera <martin.osmera@moravia-microsystems.com>
+#
+# ==============================================================================
+
+macro ( ADD_TEST_SUBJECT testName testBinary )
+    set ( testDir "${CMAKE_SOURCE_DIR}/tests" )
+
+    if ( TEST_COVERAGE )
+        set ( coverageFlag 1 )
+    else()
+        set ( coverageFlag 0 )
+    endif()
+
+    if ( TEST_MEMCHECK )
+        add_test ( NAME "${testName}"
+                   COMMAND "${testDir}/coverage.sh" "${testName}" "${coverageFlag}"
+                            valgrind --leak-check=full "--log-file=${testDir}/results/${testName}-Valgring.txt"
+                           "${testBinary}" -x "${testDir}/results/${testName}" )
+    else()
+        add_test ( NAME "${testName}"
+                   COMMAND "${testDir}/coverage.sh" "${testName}" "${coverageFlag}"
+                           "${testBinary}" -x "${testDir}/results/${testName}" )
+    endif()
+endmacro()
+
+# tests.cmake ends here
