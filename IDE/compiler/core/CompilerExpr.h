fba27a93298548aeaa26bcd115fc06bc699b97be
Erik Chalupa <erik.chalupa@gmail.com>
2014-01-07 11:50:12 +0100

    +(Compiler) C update

--- IDE/compiler/core/CompilerExpr.h
+++ IDE/compiler/core/CompilerExpr.h
@@ -51,6 +51,7 @@ class CompilerExpr : public CompilerSerializable
             OPER_BOR            = L'|', ///< Bitwise OR
             OPER_BXOR           = L'^', ///< Bitwise XOR
             OPER_BAND           = L'&', ///< Bitwise AND
+            OPER_BNOT           = L'~', ///< Bitwise NOT
             OPER_LOR            = L'⋁', ///< Logical OR
             OPER_LXOR           = L'⊕', ///< Logical XOR
             OPER_LAND           = L'⋀', ///< Logical AND
@@ -84,7 +85,16 @@ class CompilerExpr : public CompilerSerializable
             OPER_INTERVALS      = 13,   ///<
             OPER_ADD_INV        = 14,   ///<
             OPER_INT_PROM       = 15,   ///<
-            OPER_NAND           = 16    ///<
+            OPER_NAND           = 16,   ///<
+            OPER_REF            = 17,   ///< Reference
+            OPER_DEREF          = 18,   ///< Dereference
+            OPER_TERNARY        = 19,
+            OPER_COLON          = 20,
+            OPER_INDEX          = 21,
+            OPER_POST_INC       = 22,
+            OPER_POST_DEC       = 23,
+            OPER_PRE_INC        = 24,
+            OPER_PRE_DEC        = 25
         };
 
     ////    Constructors and Destructors    ////
