2912bfd6a92f296e99bef9c26f0e8c6f406000d5
Martin Madron <pomerange@seznam.cz>
2014-01-27 17:18:08 +0100

    (no comment)

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
@@ -1,5 +1,12 @@
 		device	kcpsm3
+;-------------------------
+roll	macro	num
 
+		rept	7
+			num
+		endr
+endm
+;------------------------
 		ena
 
 main:	add		S0, #2
@@ -7,6 +14,20 @@ main:	add		S0, #2
 		store	S0, @S1
 		out		S0, @S1
 		call	podp
+
+        IF          S0 != #10
+            LOAD    S5,#5
+        ELSE
+            LOAD    S6,#6
+        ENDIF
+
+        regbank         a
+        load            s5,#5h
+        regbank         b
+        load            s6,#2h
+
+		roll			s6
+
 		jump	main
 
 podp:   call	podp2
