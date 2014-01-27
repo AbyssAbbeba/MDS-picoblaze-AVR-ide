51de18d75b92b7ea62728853ee4838c19379cf05
Abyss <abyss@abyss-K52JT.(none)>
2014-01-27 15:27:21 +0100

    (no comment)

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
@@ -5,7 +5,16 @@
 main:	add		S0, #2
 		add		S1, #1
 		store	S0, @S1
+		in		s0, @s1
+		out		S0, @S1
+		call	podp
 		jump	main
+
+podp:   call	podp2
+		return
+podp2:	call	podp3
+		return
+podp3:	return
 
 int:	jump	$
 
