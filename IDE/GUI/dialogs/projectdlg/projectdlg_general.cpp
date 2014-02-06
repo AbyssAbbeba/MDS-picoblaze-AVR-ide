59e9a6c2db3fb80275e873a182a767c5e423777f
Erik Chalupa <erik.chalupa@gmail.com>
2014-02-06 18:29:53 +0100

    +(GUI) update

---
+++ IDE/GUI/dialogs/projectdlg/projectdlg_general.cpp
@@ -0,0 +1,79 @@
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * Copyright: See COPYING file that comes with this distribution.
+ *
+ * @author: Erik Chalupa <xchalu10@stud.fit.vutbr.cz>, (C) 2014
+ *
+ */
+
+
+
+#include <QtGui>
+#include "projectdlg_general.h"
+
+
+/**
+ * @brief
+ * @param
+ * @param
+ */
+Projectdlg_General::Projectdlg_General(QWidget *parent)
+    : QWidget(parent)
+{
+    //layout = new QGridLayout(this);
+    ui.setupUi(this);
+
+    connect(ui.btnChange, SIGNAL(pressed()), this, SLOT(setPath()));
+
+    this->show();
+}
+
+
+/**
+ * @brief
+ */
+void Projectdlg_General::setPath()
+{
+    QString path = QFileDialog::getExistingDirectory (this, tr("Project Directory"), "");
+    ui.lePath->setText(path);
+}
+
+
+QString Projectdlg_General::getPath()
+{
+    return ui.lePath->text();
+}
+
+
+QString Projectdlg_General::getName()
+{
+    return ui.leName->text();
+}
+
+
+QString Projectdlg_General::getArch()
+{
+    return ui.cmbArch->currentText();
+}
+
+
+QString Projectdlg_General::getFamily()
+{
+    return ui.cmbFamily->currentText();
+}
+
+
+QString Projectdlg_General::getIntVector()
+{
+    return ui.leIntVector->text();
+}
+
+
+QString Projectdlg_General::getHWBuild()
+{
+    return ui.leHWBuild->text();
+}
