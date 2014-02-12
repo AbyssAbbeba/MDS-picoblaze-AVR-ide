937e0707feed65fbd0b71d08e0693abe27b05532
Abyss <abyss@abyss-K52JT.(none)>
2014-02-12 16:15:39 +0100

    (no comment)

--- IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm3/testcases/010_store_fetch.asm
@@ -93,7 +93,40 @@ MAIN:
 
 
 
+            call        aaa
+            load        s1,#1
+aaa:
+            load        s2,#255
+            load        s1,#1
 
+            sub         s1,#1
+            load        s3,#25
+            call        Z,zero_test
+
+
+            load        s2,#255
+zero_test:
+            load        s1,#1
+            sub         s2,#12
+            call        NZ,nzero_test
+
+
+            load        s2,#255
+nzero_test:
+            load        s2,#255
+            add         s2,#10
+            call        C,c_test
+
+
+            load        s2,#255
+c_test:
+            load        s2,#255
+            add         s1,#10
+            call        NC,nc_test
+
+            load        s1,#1
+nc_test:
+            load        s1,#1
 		call	Z,podp
 		jump	main
 
---
+++ IDE/tests/simulator/MCUSim/PicoBlaze/TestKcpsm6/testcases/024_conditional_return.asm
@@ -0,0 +1,71 @@
+; TEST CASE
+; testing conditional jump and ret
+device kcpsm6
+org     0
+
+start:
+
+; jump instruction
+            jump        aaa
+            load        s1,#1
+aaa:
+            load        s2,#255
+            load        s1,#1
+
+            sub         s1,#1
+            load        s3,#25
+            jump        Z,zero_test
+                        ;; step
+                        ;; pc == 2
+                        ;; step 4
+                        ;; flag[z] == true
+                        ;; step                 # jump z
+                        ;; pc == 8
+
+            load        s2,#255
+zero_test:
+            load        s1,#1
+            sub         s2,#12
+            jump        NZ,nzero_test
+                        ;; step                 # sub
+                        ;; step                 # jump nz
+                        ;; step                 #
+                        ;; flag[z] == false
+                        ;; pc == 12
+
+            load        s2,#255
+nzero_test:
+            load        s2,#255
+            add         s2,#10
+            jump        C,c_test
+
+                        ;; step                 # add
+                        ;; pc == 13
+                        ;; step                 # jump c
+                        ;; pc == 14
+                        ;; flag[c] == true
+                        ;; pc == 14
+                        ;; step                 # after jump load2
+                        ;; pc == 16
+
+            load        s2,#255
+c_test:
+            load        s2,#255
+            add         s1,#10
+            jump        NC,nc_test
+
+                        ;; step                 #
+                        ;; pc == 17
+                        ;; step                 # jump
+                        ;; pc == 18
+                        ;; step
+                        ;; flag[c] == false
+                        ;; pc == 20
+            load        s1,#1
+nc_test:
+            load        s1,#1
+
+
+
+
+           
