45fec0526a06909e679e30a60aaa193264e8ebe3
Erik Chalupa <erik.chalupa@gmail.com>
2014-02-27 19:42:29 +0100

    +(GUI) update

---
+++ IDE/GUI/dialogs/projectcfg/projectcfg_templates.h
@@ -0,0 +1,18 @@
+#ifndef PROJECTCFG_TEMPLATES
+#define PROJECTCFG_TEMPLATES
+
+#include "../project/project.h"
+#include "ui_projectcfg_templates.h"
+
+
+class ProjectCfg_Templates : public QWidget
+{
+    public:
+        ProjectCfg_Templates(QWidget *parent);
+
+    private:
+        Ui_ProjectCfg_Templates ui;
+};
+
+
+#endif
