fa5495efcd0f35f6fd6ce88c26ed9c08d0ed7f82
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-03-17 15:49:53 +0100

    (no comment)

---
+++ IDE/utilities/LicenseCertificates/LicenseCertificates.cxx
@@ -0,0 +1,16 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * (C) copyright 2014 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup LicenseCertificates
+ * @file LicenseCertificates.cxx
+ */
+// =============================================================================
+
+#include "LicenseCertificates.h"
