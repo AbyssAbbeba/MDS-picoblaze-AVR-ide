37fe8c8553c8389269f19ae5e3faf4de0f45137f
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-07-24 08:51:02 +0200

    (no comment)

---
+++ IDE/utilities/AdjSimProcDef/AdjSimProcDefGenerator.h
@@ -0,0 +1,31 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Interface: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup AdjSimProcDef
+ * @file AdjSimProcDefGenerator.h
+ */
+// =============================================================================
+
+#ifndef ADJSIMPROCDEFGENERATOR_H
+#define ADJSIMPROCDEFGENERATOR_H
+
+#include "AdjSimProcDefGenerator.h"
+
+/**
+ * @brief
+ * @ingroup AdjSimProcDef
+ * @class AdjSimProcDefGenerator
+ */
+class AdjSimProcDefGenerator
+{
+
+};
+
+#endif // ADJSIMPROCDEFGENERATOR_H
---
+++ IDE/utilities/AdjSimProcDef/AdjSimProcDefParser.h
@@ -0,0 +1,31 @@
+// =============================================================================
+/**
+ * @brief
+ * C++ Interface: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup AdjSimProcDef
+ * @file AdjSimProcDefParser.h
+ */
+// =============================================================================
+
+#ifndef ADJSIMPROCDEFPARSER_H
+#define ADJSIMPROCDEFPARSER_H
+
+#include "AdjSimProcDef.h"
+
+/**
+ * @brief
+ * @ingroup AdjSimProcDef
+ * @class AdjSimProcDef
+ */
+class AdjSimProcDefParser
+{
+
+};
+
+#endif // ADJSIMPROCDEFPARSER_H
