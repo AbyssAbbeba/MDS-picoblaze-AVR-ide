aa98cf21c82f4364b2d7432a8ea0c98fe6a955ac
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-08-12 12:51:00 +0200

    (no comment)

--- IDE/utilities/AdjSimProcDef/AdjSimProcDef.h
+++ IDE/utilities/AdjSimProcDef/AdjSimProcDef.h
@@ -137,9 +137,113 @@ class AdjSimProcDef
             std::vector<Operand> m_operands;
         };
 
+        struct Memory
+        {
+            struct Register
+            {
+                unsigned int m_size;
+                unsigned int m_banks; //< should divide m_size
+            } m_register;
+
+            struct Data
+            {
+                unsigned int m_size;
+            } m_data;
+
+            struct Program
+            {
+                unsigned int m_size;
+
+                enum WordSize
+                {
+                    WORD_1B,
+                    WORD_2B,
+                    WORD_3B
+                } m_word;
+
+                enum Endian
+                {
+                    END_BIG,
+                    END_LITTLE
+                } m_endian;
+            } m_program;
+        };
+
+        struct Stack
+        {
+            /// Stack capacity, set to 0 when simple stack is used or no stack is used at all.
+            unsigned int m_size;
+
+            /// Use stack as designated single purpose memory accessible only by specific instruction subset.
+            bool m_useDesignatedStack;
+
+            /**
+             * @brief Settings related to simple stack, i.e. stack not implemented as designated single purpose memory.
+             */
+            struct SimpleStack
+            {
+                /// Target memory space.
+                enum Space
+                {
+                    SP_REG, ///< Register file.
+                    SP_DATA ///< Data memory.
+                };
+
+                /// Mode of stack operation.
+                enum Operation
+                {
+                    OP_PREINC,  ///< Increment SP before PUSH, decrement after  POP.
+                    OP_POSTINC, ///< Increment SP after  PUSH, decrement before POP.
+                    OP_PREDEC,  ///< Decrement SP before PUSH, increment after  POP.
+                    OP_POSTDEC  ///< Decrement SP after  PUSH, increment before POP.
+                } m_operation;
+
+                /// Location where the stacked values are stored.
+                struct Content
+                {
+                    Space m_space; ///< Memory space.
+                    int m_offset;  ///< This value is added to the stack pointer value before accessing stack memory.
+                } m_content;
+
+                /// Location designated for the stack pointer.
+                struct Pointer
+                {
+                    Space m_space;   ///< Memory space.
+                    int m_address;   ///< Address in the designated memory space.
+                    int m_maxSize; ///< Maximum allowed stack pointer value, -1 means no limit.
+                    bool m_indirect; ///< If true, "m_address" is threated as indirect address.
+                } m_pointer;
+            } m_simpleStack;
+        };
+
+        struct Ports
+        {
+            unsigned int m_addrWidth;
+            unsigned int m_dataWidth;
+        };
+
+        struct Flags
+        {
+            bool m_backupWhenInterruped;
+            bool m_autoDisableInterrputs; // When when entering ISR.
+            bool m_autoEnableInterrputs; // When when leaving ISR.
+
+            bool m_hasZero;
+            bool m_hasCarry;
+            bool m_hasOverflow;
+            bool m_hasNegative;
+            bool m_hasHalf;
+            bool m_hasParity;
+        };
+
     ////    Public Attributes    ////
     public:
         std::string m_name;
+        unsigned int m_interruptVector;
+        Ports m_ports;
+        Memory m_memory;
+        Stack m_stack;
+        Flags m_flags;
         std::vector<Instruction> m_instructionSet;
 };
 
@@ -153,6 +257,24 @@ class AdjSimProcDef
      */
     std::ostream & operator << ( std::ostream & out,
                                  const AdjSimProcDef & definition );
+
+    std::ostream & operator << ( std::ostream & out,
+                                 const AdjSimProcDef::Ports & definition );
+
+    std::ostream & operator << ( std::ostream & out,
+                                 const AdjSimProcDef::Memory & definition );
+
+    std::ostream & operator << ( std::ostream & out,
+                                 const AdjSimProcDef::Stack & definition );
+
+    std::ostream & operator << ( std::ostream & out,
+                                 const AdjSimProcDef::Flags & definition );
+
+    std::ostream & operator << ( std::ostream & out,
+                                 const AdjSimProcDef::Instruction & definition );
+
+    std::ostream & operator << ( std::ostream & out,
+                                 const AdjSimProcDef::Stack::SimpleStack::Space & definition );
 //@}
 
 #endif // ADJSIMPROCDEF_H
