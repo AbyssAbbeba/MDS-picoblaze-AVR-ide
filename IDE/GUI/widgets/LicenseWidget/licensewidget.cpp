a4a93295ebe8fc750d712496660c127a143978f6
Erik C <xchalu10@stud.fit.vutbr.cz>
2014-09-02 14:53:59 +0200

    +GUI

--- IDE/GUI/widgets/LicenseWidget/licensewidget.cpp
+++ IDE/GUI/widgets/LicenseWidget/licensewidget.cpp
@@ -18,6 +18,7 @@
 #include "../../../utilities/LicenseCertificate/LicenseCertificate.h"
 #include "../../guicfg/guicfg.h"
 #include <iostream>
+#include "../../../mds.h"
 
 
 LicenseWidget::LicenseWidget(QWidget *parent)
@@ -27,7 +28,9 @@ LicenseWidget::LicenseWidget(QWidget *parent)
     ui.setupUi(this);
     this->setLayout(ui.gridLayout);
     ui.teInfo->setReadOnly(true);
-    connect(ui.btnLoad, SIGNAL(clicked()), this, SLOT(load()));
+    #ifndef MDS_VARIANT_TRIAL
+        connect(ui.btnLoad, SIGNAL(clicked()), this, SLOT(load()));
+    #endif
     connect(ui.btnPrint, SIGNAL(clicked()), this, SLOT(print()));
     //connect(this, SIGNAL(accepted()), this, SLOT(tryAccept()));
     //connect(this, SIGNAL(rejected()), this, SLOT(tryReject()));
@@ -42,47 +45,54 @@ LicenseWidget::LicenseWidget(QWidget *parent)
 
 void LicenseWidget::tryLoad()
 {
-    this->licensePath = GuiCfg::getInstance().getLicensePath();
-    std::ifstream ifs;
+    #ifndef MDS_VARIANT_TRIAL
+        this->licensePath = GuiCfg::getInstance().getLicensePath();
+        std::ifstream ifs;
 
-    ifs.open(this->licensePath.toStdString(), std::ios_base::binary);
+        ifs.open(this->licensePath.toStdString(), std::ios_base::binary);
 
-    if (ifs.is_open())
-    {
-        //load info
-        LicenseCertificate crt(ifs);
-        if ( true == crt.m_isValid )
+        if (ifs.is_open())
         {
-            this->license = true;
-            ui.teInfo->clear();
-            QFile file(":/resources/html/license.html");
-            if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+            //load info
+            LicenseCertificate crt(ifs);
+            if ( true == crt.m_isValid )
             {
-                ui.teInfo->setHtml(QString::fromUtf8(file.readAll()));
-                QTextCursor cur(ui.teInfo->document());
-                cur = ui.teInfo->document()->find("__LICENSES__");
-                cur.insertText(QString::fromUtf8(crt.m_product.m_licences.c_str()));
-                cur = ui.teInfo->document()->find("__TRIAL__");
-                cur.insertText("30");
-                cur = ui.teInfo->document()->find("__NAME__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_name.c_str()));
-                cur = ui.teInfo->document()->find("__EMAIL__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_email.c_str()));
-                cur = ui.teInfo->document()->find("__PHONE__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_phone.c_str()));
-                cur = ui.teInfo->document()->find("__STREET__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_street.c_str()));
-                cur = ui.teInfo->document()->find("__NUMBER__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_number.c_str()));
-                cur = ui.teInfo->document()->find("__POST__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_post.c_str()));
-                cur = ui.teInfo->document()->find("__CITY__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_municipality.c_str()));
-                cur = ui.teInfo->document()->find("__STATE__");
-                cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_country.c_str()));
-                ui.teInfo->setTextCursor(cur);
-                file.close();
-                this->setResult(QDialog::Accepted);
+                this->license = true;
+                ui.teInfo->clear();
+                QFile file(":/resources/html/license.html");
+                if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+                {
+                    ui.teInfo->setHtml(QString::fromUtf8(file.readAll()));
+                    QTextCursor cur(ui.teInfo->document());
+                    cur = ui.teInfo->document()->find("__LICENSES__");
+                    cur.insertText(QString::fromUtf8(crt.m_product.m_licences.c_str()));
+                    cur = ui.teInfo->document()->find("__NAME__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_name.c_str()));
+                    cur = ui.teInfo->document()->find("__EMAIL__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_email.c_str()));
+                    cur = ui.teInfo->document()->find("__PHONE__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_phone.c_str()));
+                    cur = ui.teInfo->document()->find("__STREET__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_street.c_str()));
+                    cur = ui.teInfo->document()->find("__NUMBER__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_number.c_str()));
+                    cur = ui.teInfo->document()->find("__POST__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_post.c_str()));
+                    cur = ui.teInfo->document()->find("__CITY__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_municipality.c_str()));
+                    cur = ui.teInfo->document()->find("__STATE__");
+                    cur.insertText(QString::fromUtf8(crt.m_licensee.m_address.m_country.c_str()));
+                    ui.teInfo->setTextCursor(cur);
+                    file.close();
+                    this->setResult(QDialog::Accepted);
+                }
+            }
+            else
+            {
+                ui.teInfo->clear();
+                this->license = false;
+                ui.teInfo->insertPlainText("<invalid certificate>");
+                this->setResult(QDialog::Rejected);
             }
         }
         else
@@ -92,14 +102,22 @@ void LicenseWidget::tryLoad()
             ui.teInfo->insertPlainText("<invalid certificate>");
             this->setResult(QDialog::Rejected);
         }
-    }
-    else
-    {
+    #endif
+    #ifdef MDS_VARIANT_TRIAL
+        this->license = true;
         ui.teInfo->clear();
-        this->license = false;
-        ui.teInfo->insertPlainText("<invalid certificate>");
-        this->setResult(QDialog::Rejected);
-    }
+        QFile file(":/resources/html/license.html");
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            ui.teInfo->setHtml(QString::fromUtf8(file.readAll()));
+            QTextCursor cur(ui.teInfo->document());
+            cur = ui.teInfo->document()->find("__TRIAL__");
+            cur.insertText(QString::number(MDS_TRIAL_PERIOD));
+            ui.teInfo->setTextCursor(cur);
+            file.close();
+        }
+        this->setResult(QDialog::Accepted);
+    #endif
 }
 
 
