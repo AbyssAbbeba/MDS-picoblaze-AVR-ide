491532a340bc81263756271ae4d040d142b9d16d
Martin Madron <pomerange@seznam.cz>
2014-09-12 16:15:49 +0200

    (no comment)

--- IDE/GUI/resources/projects/MDSExample/Example1.asm
+++ IDE/GUI/resources/projects/MDSExample/Example1.asm
@@ -4,12 +4,15 @@
 ; Press F2 and F6 to run the program (start simulator and animate)
 
         org     0h
-
-main:   inc     s1
+        load    s0,#1
+main:   
         store   s0,@s1
+        inc     s1
         
-        if      s1 == #64
+        
+        if      s1 == #255
             load     s1, #0
+            inc      s0
             jump     main
         else
             jump     main   
