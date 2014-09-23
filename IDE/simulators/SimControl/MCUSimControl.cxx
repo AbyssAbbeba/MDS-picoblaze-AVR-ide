faf1aabf806aa81298fdad30864794b24c1c45d8
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-09-23 15:41:48 +0200

    (no comment)

--- IDE/simulators/SimControl/MCUSimControl.cxx
+++ IDE/simulators/SimControl/MCUSimControl.cxx
@@ -242,9 +242,9 @@ bool MCUSimControl::startSimulation ( const std::string & filename,
         case MCUSim::ARCH_PIC8:
             dynamic_cast<PIC8ProgramMemory*>(m_simulator->getSubsys(MCUSimSubsys::ID_MEM_CODE))->loadDataFile(dataFile);
             break;
-      #ifdef MDS_FEATURE_PIC8
+      #endif // MDS_FEATURE_PIC8
 
-      #endif // MDS_FEATURE_PICOBLAZE
+      #ifdef MDS_FEATURE_PICOBLAZE
         case MCUSim::ARCH_PICOBLAZE:
             dynamic_cast<PicoBlazeProgramMemory*>(m_simulator->getSubsys(MCUSimSubsys::ID_MEM_CODE))->loadDataFile(dataFile);
             break;
