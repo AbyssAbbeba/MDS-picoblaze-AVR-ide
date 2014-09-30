a84eaab40cff85a39c2a6fb8e0989854bd4b3bfa
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-09-30 14:00:31 +0200

    (no comment)

--- IDE/compiler/modules/assembler/Adaptable/AsmAdaptableSemanticAnalyzer.cxx
+++ IDE/compiler/modules/assembler/Adaptable/AsmAdaptableSemanticAnalyzer.cxx
@@ -8,9 +8,17 @@
  * (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
  *
  * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
- * @ingroup Assembler
+ * @ingroup AdaptableAsm
  * @file AsmAdaptableSemanticAnalyzer.cxx
  */
 // =============================================================================
 
 #include "AsmAdaptableSemanticAnalyzer.h"
+
+void AsmAdaptableSemanticAnalyzer::process ( CompilerStatement * codeTree )
+{
+}
+
+void AsmAdaptableSemanticAnalyzer::setDevice ( const std::string & deviceName )
+{
+}
--- IDE/compiler/modules/assembler/Adaptable/AsmAdaptableSemanticAnalyzer.h
+++ IDE/compiler/modules/assembler/Adaptable/AsmAdaptableSemanticAnalyzer.h
@@ -8,12 +8,53 @@
  * (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
  *
  * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
- * @ingroup AdjSimProcDef
+ * @ingroup AdaptableAsm
  * @file AsmAdaptableSemanticAnalyzer.h
  */
 // =============================================================================
 
 #ifndef ASMADAPTABLESEMANTICANALYZER_H
 #define ASMADAPTABLESEMANTICANALYZER_H
+
+// Common compiler header files.
+#include "CompilerSemanticAnalyzer.h"
+#include "AsmMachineCodeGen.h"
+
+/**
+ * @brief
+ * @ingroup PicoBlazeAsm
+ * @class AsmAdaptableSemanticAnalyzer
+ */
+class AsmAdaptableSemanticAnalyzer : public CompilerSemanticAnalyzer
+{
+    ////    Constructors and Destructors    ////
+    public:
+        /**
+         * @brief
+         * @param[in,out] compilerCore
+         * @param[in] opts
+         */
+        AsmAdaptableSemanticAnalyzer ( CompilerSemanticInterface * compilerCore,
+                                       CompilerOptions * opts );
+
+        /**
+         * @brief
+         */
+        virtual ~AsmAdaptableSemanticAnalyzer();
+
+    ////    Public Operations    ////
+    public:
+        /**
+         * @brief
+         * @param[in,out] codeTree
+         */
+        virtual void process ( CompilerStatement * codeTree ) override;
+
+        /**
+         * @brief
+         * @param[in] deviceName
+         */
+        virtual void setDevice ( const std::string & deviceName ) override;
+};
 
 #endif // ASMADAPTABLESEMANTICANALYZER_H
