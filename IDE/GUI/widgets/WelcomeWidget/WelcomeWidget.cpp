dab8ddd7cf00796b640bd9bc60d03c761281f16c
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-10-23 15:15:37 +0200

    (no comment)

---
+++ IDE/GUI/widgets/WelcomeWidget/WelcomeWidget.cpp
@@ -0,0 +1,24 @@
+/**
+ * @brief
+ * C++ Implementation: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
+ *
+ * @author Martin Ošmera <martin.osmera@moravia-microsystems.com>
+ * @ingroup GUI
+ * @file WelcomeWidget.cpp
+ */
+
+#include "WelcomeWidget.h"
+
+WelcomeWidget::WelcomeWidget ( QWidget * parent ) : QWidget(parent)
+{
+    setupUi(this);
+}
+
+inline void WelcomeWidget::setupConnections()
+{
+    connect(pushButtonDismiss, SIGNAL(clicked()), SLOT(close()));
+}
