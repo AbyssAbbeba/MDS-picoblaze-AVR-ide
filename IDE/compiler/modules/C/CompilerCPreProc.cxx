f427dcb5a9ff2f2ecd96d54c75b73960ba22d62f
Martin Osmera <martin.osmera@moravia-microsystems.com>
2014-12-23 14:00:57 +0100

    (no comment)

--- IDE/compiler/modules/C/CompilerCPreProc.cxx
+++ IDE/compiler/modules/C/CompilerCPreProc.cxx
@@ -55,19 +55,19 @@ char * CompilerCPreProc::processFiles ( const std::vector<FILE*> & inputFiles )
     // Start in the Normal mode.
     m_inmode = MODE_NORMAL;
 
-    m_parserLocation.m_fileNumber = 0;
-    m_parserLocation.m_lineStart = 0;
-    m_parserLocation.m_lineEnd = 0;
-    m_parserLocation.m_colStart = 0;
-    m_parserLocation.m_colEnd = 0;
-    m_parserLocation.m_origin = 0;
+    m_location.m_fileNumber = 0;
+    m_location.m_lineStart = 0;
+    m_location.m_lineEnd = 0;
+    m_location.m_colStart = 0;
+    m_location.m_colEnd = 0;
+    m_location.m_origin = 0;
 
-    m_preProcLocation.m_fileNumber = 0;
-    m_preProcLocation.m_lineStart = 0;
-    m_preProcLocation.m_lineEnd = 0;
-    m_preProcLocation.m_colStart = 0;
-    m_preProcLocation.m_colEnd = 0;
-    m_preProcLocation.m_origin = 0;
+//     m_locationStack.m_fileNumber = 0;
+//     m_locationStack.m_lineStart = 0;
+//     m_locationStack.m_lineEnd = 0;
+//     m_locationStack.m_colStart = 0;
+//     m_locationStack.m_colEnd = 0;
+//     m_locationStack.m_origin = 0;
 
 
     // Iterate over all given input files.
