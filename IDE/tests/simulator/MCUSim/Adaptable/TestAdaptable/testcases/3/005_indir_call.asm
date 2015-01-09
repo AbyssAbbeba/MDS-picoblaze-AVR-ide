952b0b18b234dc6ef583e48741b56aaf29c2c275
Abyss <abyss@abyss-K52JT.(none)>
2015-01-09 18:17:02 +0100

    (no comment)

---
+++ IDE/tests/simulator/MCUSim/Adaptable/TestAdaptable/testcases/3/005_indir_call.asm
@@ -0,0 +1,26 @@
+
+
+device kcpsm6
+
+org             0
+
+start:
+        LOAD sA, #0b00000001
+        LOAD sB, #0b00000001
+        ;; step 2
+        ;; pc == 2
+        CALL@ (sA, sB)
+        ;; step
+        ;; pc == 257
+        jump    $
+
+ org            0b100000001
+        LOAD    s0,#11
+        RETURN
+        
+       
+
+        ;; step
+        ;; pc == 258
+        ;; reg[0] == 11
+        ;; stack[0] == 3
---
+++ IDE/tests/simulator/MCUSim/Adaptable/TestAdaptable/testcases/3/009_cmp_test.asm
@@ -0,0 +1,36 @@
+; testcase for instructions compare and TEST
+
+device kcpsm6
+    ORG         0x000
+
+
+Start:
+
+            LOAD        s0,#20
+            LOAD        s1,#1
+            LOAD        s2,#50
+            LOAD        s3,#50
+            LOAD        s4,#20
+            LOAD        s5,#1
+            ;; step 6
+            ;; reg[0]==20
+            ;; reg[1]==1
+            ;; reg[2]==50
+            ;; reg[3]==50
+            ;; reg[4]==20
+            ;; reg[5]==1
+            COMPARE     s0,#20
+            ;; step
+            ;; interrupt
+            ;; flag[z] != false
+            ;; flag[c] != true
+            ;; flag[pz] == false
+            ;; flag[pc] == false
+            ;; flag[i] == false
+            ;; flag[ie] == false
+            ;; pc == 7
+            ;; step
+            ;; pc == 8
+            
+            
+        
