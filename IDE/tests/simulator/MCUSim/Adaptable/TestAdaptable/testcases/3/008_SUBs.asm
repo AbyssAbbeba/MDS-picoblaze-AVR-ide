8f6e30f35ca32757e57a6b67185a253c83783995
Martin Madron <pomerange@seznam.cz>
2015-01-09 18:17:02 +0100

    (no comment)

---
+++ IDE/tests/simulator/MCUSim/Adaptable/TestAdaptable/testcases/3/008_SUBs.asm
@@ -0,0 +1,50 @@
+
+device kcpsm6
+ORG 0
+
+
+START:
+
+;sA = 4B which is not zero (Z=0) and with no underflow (C=0).
+;sA = 00 which is zero (Z=1) but there was no underflow (C=0).
+;8E – B5 = 1D9
+;sA = D9 which is not zero (Z=0) but there was an underflow (C=1).
+;This is equivalent to 142 – 181 = -39 where D9 hex is the twos complement representation of -39.
+;However, it is the users responsibility to implement and interpret signed arithmetic values and operations
+LOAD sA, #8Eh                    ;8E - 43 = 4B
+SUB sA, #43h                      ;
+LOAD sA, #8Eh                     ;8E - 8E = 00
+SUB sA, sA
+LOAD sA, #8Eh
+SUB sA, #B5h
+                                ;; step
+                                ;; reg[10] == 0x8E
+                                ;; step
+                                ;; flag[z] == false
+                                ;; flag[c] == false
+                                ;; step 2
+                                ;; flag[z] == true
+                                ;; flag[c] == false
+                                ;; step 2
+                                ;; flag[z] == false
+                                ;; flag[c] == true
+                                
+;Carry from previous operation [sB, sA] = A2 7B - A1 B9 = 00C2 7B – B9 = (-)C2 A2 – A1 - 1 = 00
+;sA = C2, Z=0, C=1.
+;sB = 00, Z=0, C=0.
+;LOAD sA, 7B
+;LOAD sB, A2
+;SUB sA, B9
+;SUBCY sB, A1
+
+
+
+;Zero from previous operation
+;[sB, sA] = A2 7B - A2 7B = 0000
+;7B – 7B = 00 sA = 00, Z=1, C=0.
+;A2 – A2 - 0 = 00 sB = 00, Z=1, C=0.
+;LOAD sA, 7B
+;LOAD sB, A2
+;SUB sA, sA
+;SUBCY sB, sB
+
---
+++ IDE/tests/simulator/MCUSim/Adaptable/TestAdaptable/testcases/3/013_SL_all.asm
@@ -0,0 +1,196 @@
+    ; testing of rotate instructions
+
+device kcpsm6
+    org 00h
+
+start:      load        s0,#1
+            load        s1,#1
+            load        s2,#1
+            load        s3,#0
+            ;; step     4
+            ;; reg[0] == 1
+            ;; reg[1] == 1
+            ;; reg[2] == 1
+            ;; reg[3] == 0
+; sl0 testing
+            sl0         s0
+            ;; step
+            ;; reg[0] == 2
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl0         s0
+            ;; step
+            ;; reg[0] == 4
+            ;; flag[c] == false
+            ;; flag[z] == false
+           
+            sl0         s0
+            ;; step
+            ;; reg[0] == 8
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl0         s0
+            ;; step
+            ;; reg[0] == 16
+            ;; flag[c] == false
+            ;; flag[z] == false
+           
+            sl0         s0
+            ;; step
+            ;; reg[0] == 32
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl0         s0
+            ;; step
+            ;; reg[0] == 64
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl0         s0
+            ;; step
+            ;; reg[0] == 128
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl0         s0
+            ;; step
+            ;; reg[0] == 0
+            ;; flag[c] == true
+            ;; flag[z] == true
+
+; sl1 testing
+            sl1         s0
+            ;; step
+            ;; reg[0] == 1
+            sl1         s0
+            ;; step
+            ;; reg[0] == 3
+            sl1         s0
+            ;; step
+            ;; reg[0] == 7
+            sl1         s0
+            ;; step
+            ;; reg[0] == 15
+            sl1         s0
+            ;; step
+            ;; reg[0] == 31
+            sl1         s0
+            ;; step
+            ;; reg[0] == 63
+            sl1         s0
+            ;; step
+            ;; reg[0] == 127
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl1         s0
+            ;; step
+            ;; reg[0] == 255
+            ;; flag[c] == false
+            ;; flag[z] == false
+            sl1         s0
+            ;; step
+            ;; reg[0] == 255
+            ;; flag[c] == true
+            ;; flag[z] == false
+; slx testing
+            load        s2,#1                
+            slx         s2
+            ;; step 2
+            ;; reg[2] == 3          
+            slx         s2
+            ;; step
+            ;; reg[2] == 7
+            slx         s2
+            ;; step
+            ;; reg[2] == 15
+            slx         s2
+            ;; step
+            ;; reg[2] == 31
+            slx         s2
+            ;; step
+            ;; reg[2] == 63
+            slx         s2
+            ;; step
+            ;; reg[2] == 127
+            ;; flag[c] == false
+            ;; flag[z] == false
+            slx         s2
+            ;; step
+            ;; reg[0] == 255
+            ;; flag[c] == false
+            ;; flag[z] == false
+            slx         s2
+            ;; step
+            ;; reg[2] == 255
+            ;; flag[c] == true
+            ;; flag[z] == false
+            load        s2,#128
+            slx         s2
+            ;; step 2
+            ;; flag[c] == true
+            ;; flag[z] == true  
+; sla testing
+            sla         s3
+            ;; step     
+            ;; reg[3] == 1
+            sla         s3
+            ;; step
+            ;; reg[3] == 2
+            sla         s3
+            ;; step
+            ;; reg[3] == 4
+            sla         s3
+            ;; step
+            ;; reg[3] == 8
+            sla         s3
+            ;; step
+            ;; reg[3] == 16
+            sla         s3
+            ;; step
+            ;; reg[3] == 32
+            sla         s3
+            ;; step
+            ;; reg[3] == 64
+            sla         s3
+            ;; step
+            ;; reg[3] == 128
+            sla         s3
+            ;; step
+            ;; reg[3] == 0
+            ;; flag[z] == true
+            ;; flag[c] == true
+            sla         s3
+            ;; step
+            ;; reg[3] == 1
+            ;; flag[z] == false
+            ;; flag[c] == false
+; testing of rl
+            rl         s3
+            ;; step
+            ;; reg[3] == 2
+            rl         s3
+            ;; step
+            ;; reg[3] == 4
+            rl         s3
+            ;; step
+            ;; reg[3] == 8
+            rl         s3
+            ;; step
+            ;; reg[3] == 16
+            rl         s3
+            ;; step
+            ;; reg[3] == 32
+            rl         s3
+            ;; step
+            ;; reg[3] == 64
+            rl         s3
+            ;; step
+            ;; reg[3] == 128
+            rl         s3
+            ;; step
+            ;; reg[3] == 1
+            ;; flag[c] == true
+            ;; flag[z] == false
+            load        s3,#0
+            rl          s3
+            ;; step 2
+            ;; flag[z] == true
+            ;; flag[c] == false
