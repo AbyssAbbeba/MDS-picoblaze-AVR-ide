271ab051a90aaa6dfad013d89c4815184c9c3d83
Martin Madron <pomerange@seznam.cz>
2015-01-12 22:23:11 +0100

    (no comment)

--- IDE/tests/simulator/MCUSim/Adaptable/TestAdaptable/testcases/3/023_conditional_call.asm
+++ IDE/tests/simulator/MCUSim/Adaptable/TestAdaptable/testcases/3/023_conditional_call.asm
@@ -32,7 +32,7 @@ aaa:
 
             sub         s1,#1
             load        s3,#25
-            call        Z,zero_test
+            callZ       zero_test
                         ;; step
                         ;; pc == 2
                         ;; step 4
@@ -44,7 +44,7 @@ aaa:
 zero_test:
             load        s1,#1
             sub         s2,#12
-            call        NZ,nzero_test
+            callNZ      nzero_test
                         ;; step                 # sub
                         ;; step                 # call nz
                         ;; step                 #
@@ -55,7 +55,7 @@ zero_test:
 nzero_test:
             load        s2,#255
             add         s2,#10
-            call        C,c_test
+            callC       c_test
 
                         ;; step                 # add
                         ;; pc == 13
@@ -70,7 +70,7 @@ nzero_test:
 c_test:
             load        s2,#255
             add         s1,#10
-            call        NC,nc_test
+            callNC      nc_test
 
                         ;; step                 #
                         ;; pc == 17
