f7c019317ed07f5f161ef6e018d136a79bea5ee4
Erik C <xchalu10@stud.fit.vutbr.cz>
2015-01-15 20:18:32 +0100

    +gui

---
+++ IDE/GUI/widgets/CallWatcher/callitemwidget.h
@@ -0,0 +1,37 @@
+/**
+ * @brief
+ * C++ Interface: ...
+ *
+ * ...
+ *
+ * (C) copyright 2013, 2014 Moravia Microsystems, s.r.o.
+ *
+ * @author Erik Chalupa <erik.chalupa@moravia-microsystems.com>
+ * @ingroup GUI
+ * @file callwatcher.h
+ */
+
+
+#ifndef CALLITEMWIDGET_H
+#define CALLITEMWIDGET_H
+
+
+#include <QWidget>
+#include <QString>
+#include "ui_callitemwidget.h"
+
+
+
+class CallItemWidget : public QWidget
+{
+    public:
+        CallItemWidget(QWidget *parent, QString event, int from, int to);
+
+    private:
+        Ui_CallItemWidget ui;
+};
+
+
+
+
+#endif
