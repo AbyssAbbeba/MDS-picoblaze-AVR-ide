d56a2780d22c51e4ed113f25a2257a6bcce03a75
Erik C <xchalu10@stud.fit.vutbr.cz>
2015-01-27 20:51:38 +0100

    +gui

--- IDE/GUI/widgets/RegWatcher/regwatcherdialog.cpp
+++ IDE/GUI/widgets/RegWatcher/regwatcherdialog.cpp
@@ -15,7 +15,8 @@
 #include <QtGui>
 #include "regwatcherdialog.h"
 
-RegWatcherDialog::RegWatcherDialog(QWidget *parent, bool regbanks, int offset)
+
+RegWatcherDialog::RegWatcherDialog(QWidget *parent, bool regbanks)
     : QDialog(parent)
 {
     ui.setupUi(this);
@@ -32,10 +33,39 @@ RegWatcherDialog::RegWatcherDialog(QWidget *parent, bool regbanks, int offset)
     }
 
     m_regbanks = regbanks;
-    m_bankOffset = offset;
     
     QRegExpValidator *validatorAddress = new QRegExpValidator(QRegExp("0x[\\da-fA-F]+"), ui.leAddress);
     ui.leAddress->setValidator(validatorAddress);
+    ui.leName->setFocus(Qt::OtherFocusReason);
+}
+
+
+RegWatcherDialog::RegWatcherDialog(QWidget *parent, bool regbanks, QString name, int address, int type, int regbank)
+    : QDialog(parent)
+{
+    ui.setupUi(this);
+    connect(this, SIGNAL(accepted()), this, SLOT(emitEditItem()));
+    connect(ui.leAddress, SIGNAL(textEdited(const QString&)), this, SLOT(checkIfValid()));
+    connect(ui.leName, SIGNAL(textEdited(const QString&)), this, SLOT(checkIfValid()));
+    connect(ui.cmbType, SIGNAL(currentIndexChanged(int)), this, SLOT(updateBanks(int)));
+    ui.btnBox->button(QDialogButtonBox::Ok)->setEnabled(false);
+
+    if (false == regbanks)
+    {
+        ui.lblBank->hide();
+        ui.cmbBank->hide();
+    }
+
+    m_regbanks = regbanks;
+
+    QRegExpValidator *validatorAddress = new QRegExpValidator(QRegExp("0x[\\da-fA-F]+"), ui.leAddress);
+    ui.leAddress->setValidator(validatorAddress);
+    ui.leName->setFocus(Qt::OtherFocusReason);
+
+    ui.leName->setText(name);
+    ui.leAddress->setText("0x" + QString::number(address, 16));
+    ui.cmbType->setCurrentIndex(type);
+    ui.cmbBank->setCurrentIndex(regbank);
 }
 
 
@@ -44,12 +74,27 @@ void RegWatcherDialog::emitNewItem()
     if (true == m_regbanks)
     {
         bool ok;
-        emit newItem(ui.leName->text(), ui.cmbType->currentIndex(), ui.leAddress->text().toInt(&ok, 16)+ui.cmbBank->currentIndex()*m_bankOffset);
+        emit newItem(ui.leName->text(), ui.cmbType->currentIndex(), ui.leAddress->text().toInt(&ok, 16), ui.cmbBank->currentIndex());
     }
     else
     {
         bool ok;
         emit newItem(ui.leName->text(), ui.cmbType->currentIndex(), ui.leAddress->text().toInt(&ok, 16));
+    }
+}
+
+
+void RegWatcherDialog::emitEditItem()
+{
+    if (true == m_regbanks)
+    {
+        bool ok;
+        emit editItem(ui.leName->text(), ui.cmbType->currentIndex(), ui.leAddress->text().toInt(&ok, 16), ui.cmbBank->currentIndex());
+    }
+    else
+    {
+        bool ok;
+        emit editItem(ui.leName->text(), ui.cmbType->currentIndex(), ui.leAddress->text().toInt(&ok, 16));
     }
 }
 
