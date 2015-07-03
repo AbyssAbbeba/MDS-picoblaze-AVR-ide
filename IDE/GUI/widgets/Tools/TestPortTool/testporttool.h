dacb0303d31c1c3588c67d84cc3473ca97a11099
Erik Chalupa <erik.chalupa@gmail.com>
2015-07-03 15:18:36 +0200

    +gui

--- IDE/GUI/widgets/Tools/TestPortTool/testporttool.h
+++ IDE/GUI/widgets/Tools/TestPortTool/testporttool.h
@@ -3,17 +3,22 @@
 
 #include <QWidget>
 #include <QtGui>
+#include "../../../../simulators/SimControl/MCUSimControl.h" //8
+#include "../../../../simulators/SimControl/MCUSimObserver.h"//8
+#include "../../../../simulators/MCUSim/MCUSim.h"            //8
+#include "../../../../simulators/MCUSim/MCUSimPureLogicIO.h" //8
+#include "ui_testporttool.h"
 
 namespace Ui {
 class TestPortTool;
 }
 
-class TestPortTool : public QWidget
+class TestPortTool : public QWidget, public MCUSimObserver
 {
     Q_OBJECT
     
 public:
-    explicit TestPortTool(QWidget *parent = 0);
+    explicit TestPortTool(QWidget *parent, MCUSimControl *controlUnit);
     ~TestPortTool();
     bool status;
     QStringList fileBuffer;
@@ -27,15 +32,22 @@ private slots:
     void readFileClicked();
     void outFileClicked();
     void inputEvent();
+    void handleUpdateRequest(int mask);
 
 private:
+    void handleEvent(int subsysId, int eventId, int locationOrReason, int detail);
+    void deviceChanged();
+    void deviceReset();
+    void setReadOnly(bool readOnly);
+    void fillBuffer();
+    int randInt(int,int);
+
     QStringList address, data;
     QFile inFile,outFile;
     QString loadPath;
     Ui::TestPortTool *ui;
-
-    void fillBuffer();
-    int randInt(int,int);
+    MCUSimControl *m_simControlUnit; //8
+    MCUSimPureLogicIO * m_plio; //8
 };
 
 #endif // TESTPORTTOOL_H
